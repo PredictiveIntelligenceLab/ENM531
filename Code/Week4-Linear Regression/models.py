bc07393363e95c94e8bfeaa796ed0e5df4adc3b8
Paris Perdikaris <>
2021-02-11 16:18:46 -0500

    Lecture #7 code

--- Code/Week4-Linear Regression/models.py
+++ Code/Week4-Linear Regression/models.py
@@ -16,39 +16,39 @@ class BayesianLinearRegression:
     P(y|x,w) ~ N(y|(w.T)*x,alpha^(-1)I)
   """
   def __init__(self, X, y, alpha = 1.0, beta = 1.0):
-      
+
       self.X = X
       self.y = y
-      
+
       self.alpha = alpha
       self.beta = beta
-      
-      self.jitter = 1e-8
-      
 
-  def fit_MLE(self): 
+      self.jitter = 1e-8
+
+
+  def fit_MLE(self):
       xTx_inv = np.linalg.inv(np.matmul(self.X.T,self.X) + self.jitter)
       xTy = np.matmul(self.X.T, self.y)
       w_MLE = np.matmul(xTx_inv, xTy)
-      
+
       self.w_MLE = w_MLE
-      
+
       return w_MLE
-      
-  def fit_MAP(self): 
+
+  def fit_MAP(self):
       Lambda = np.matmul(self.X.T,self.X) + \
                (self.beta/self.alpha)*np.eye(self.X.shape[1])
       Lambda_inv = np.linalg.inv(Lambda)
       xTy = np.matmul(self.X.T, self.y)
       mu = np.matmul(Lambda_inv, xTy)
-      
+
       self.w_MAP = mu
       self.Lambda_inv = Lambda_inv
-      
+
       return mu, Lambda_inv
-      
+
   def predictive_distribution(self, X_star):
       mean_star = np.matmul(X_star, self.w_MAP)
-      var_star = 1.0/self.alpha + \
+      var_star = np.eye(X_star.shape[0])/self.alpha + \
                  np.matmul(X_star, np.matmul(self.Lambda_inv, X_star.T))
       return mean_star, var_star
